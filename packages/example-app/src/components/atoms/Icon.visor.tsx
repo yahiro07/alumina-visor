03306c626c2d731a92e33c4885d453dc545c6032
yahiro <yahiro07@users.noreply.github.com>
2021-12-15 10:06:22 +0900

    add icon component

---
+++ packages/example-app/src/components/atoms/Icon.visor.tsx
@@ -0,0 +1,7 @@
+import { jsx } from 'alumina';
+import { Icon } from '~/components/atoms/Icon';
+
+export default {
+  base: () => <Icon spec="fa fa-cog" size={20} />,
+  large: () => <Icon spec="fa fa-wrench" size={40} />,
+};
